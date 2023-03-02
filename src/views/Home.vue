92e591de0138a7f41ea467dc72bb74431a270149
Devajohnson.P <devajohnson.pushbaraj@crud-operations.com>
2023-03-02 17:41:37 +0530

    initial_phase

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,33 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <input type="text" @change="getText" />
+    <button type="submit" @click="reversText">Press</button>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
   name: "Home",
-  components: {
-    HelloWorld,
+  data(){
+    return{
+      input_text: ""
+    }
   },
+  methods:{
+    getText(e){
+      this.input_text=e.target.value
+    console.log(this.input_text.split(" "),"input_text")
+    },
+    reversText(){
+      const textArray= this.input_text.split(" ")
+      console.log(textArray,'textArray');
+      // for(let i=0;i<textArray.length;i++){
+      //   textArray[i]=textArray[i].split("").reverse().join("")
+      //   console.log(textArray[i],"loop")
+      // }
+      console.log(textArray.reverse().join(" "),"final");
+
+    }
+  }
 };
 </script>
